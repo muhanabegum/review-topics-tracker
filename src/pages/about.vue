efb33bbb4dbcd6a2a76830455da33031b5c15229
Muhana Begum <muhana.begum@hotmail.com>
2020-08-16 01:03:30 -0400

    Update about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -8,9 +8,7 @@
           or the exam has passed, click the done button and you are officially free from studying! </p>
         <p> This web page is similar to study sheets that are typically created using paper or some sticky notes. If you aren't used to using something 
           like this, I strongly recommend you try it out and I hope everyone enjoys using it! </p>
-          <a :href="'//' + www.github.com/muhanabegum" target="_blank">
-  {{ www.github.com/muhanabegum }}
-</a>
+        <p> Author: Muhana Begum </p>
     </div>
   </div>
 </template>
